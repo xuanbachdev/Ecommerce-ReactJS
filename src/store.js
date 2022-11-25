5f544da86d24984b6bc446c962561369e3decf1f
Xuân Bách <xuanbachdevops@gmail.com>
2022-11-25 13:57:05 +0700

    add store redux

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import {configureStore} from '@reduxjs/toolkit'
+import counterProduct from '~/reducer/totalProductSlice'
+import amountProduct from '~/reducer/amountSlice'
+import favourite from '~/reducer/favourite'
+import oneProduct from '~/reducer/data1Product'
+
+export const store = configureStore({
+    reducer: {
+       counterProduct,
+       amountProduct,
+       favourite,
+       oneProduct
+    }
+})
