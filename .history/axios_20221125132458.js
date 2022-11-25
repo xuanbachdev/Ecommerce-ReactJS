dc31e9dda2c087126b2b853b1af0a138ce60b3fc
Xuân Bách <xuanbachdevops@gmail.com>
2022-11-25 13:43:56 +0700

    config axios

--- .history/axios_20221125132458.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-let token = localStorage.getItem('Token')
-axios.defaults.baseURL = 'https://shope-b3.thaihm.site/api';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Authorization'] = `${token}`
-
-export default axios
