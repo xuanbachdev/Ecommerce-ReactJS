06e056985242f02d6b211a6b083560f931f92c4c
Xuân Bách <xuanbachdevops@gmail.com>
2022-11-25 16:23:43 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-// import {BrowserRouter ,Routes,Route} from 'react-router-dom'
-// import {pagePrivate, pageRoutes} from '~/routes'
+import {BrowserRouter ,Routes,Route} from 'react-router-dom'
+import {pagePrivate, pageRoutes} from '~/routes'
 
-// import DefaultLayout from '~/Layout/DefaultLayout/DefaultLayout';
-// import { PrivateRouter } from '~/page/PrivateRouter/privateRouter';
+import DefaultLayout from '~/Layout/DefaultLayout/DefaultLayout';
+import { PrivateRouter } from '~/page/PrivateRouter/privateRouter';
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//         <Routes>
-//           {pageRoutes.map(function (route,index) {
-//               return (
-//                 <Route
-//                   key={index}
-//                   path={route.path}
-//                   element={
-//                     <DefaultLayout>
-//                         <route.component/>
-//                     </DefaultLayout>
-//                   }
-//                 />
-//               )
-//             })
-//           }
-//           {pagePrivate.map(function (routes,index) {
-//               return (
-//                 <Route
-//                   key={index}
-//                   path={routes.path}
-//                   element={
-//                     <DefaultLayout>
-//                       <PrivateRouter>
-//                         <routes.component/>
-//                       </PrivateRouter>
-//                     </DefaultLayout>
-//                   }
-//                 />
-//               )
-//             })
-//           }
-//         </Routes>
-//     </BrowserRouter>
-//   );
-// }
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          {pageRoutes.map(function (route,index) {
+              return (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <DefaultLayout>
+                        <route.component/>
+                    </DefaultLayout>
+                  }
+                />
+              )
+            })
+          }
+          {pagePrivate.map(function (routes,index) {
+              return (
+                <Route
+                  key={index}
+                  path={routes.path}
+                  element={
+                    <DefaultLayout>
+                      <PrivateRouter>
+                        <routes.component/>
+                      </PrivateRouter>
+                    </DefaultLayout>
+                  }
+                />
+              )
+            })
+          }
+        </Routes>
+    </BrowserRouter>
+  );
+}
 
-// export default App
+export default App
