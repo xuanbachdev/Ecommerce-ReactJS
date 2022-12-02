4d88c8ff5fed9f03263d1904f153f1b83fbe36a8
Xuân Bách <xuanbachdevops@gmail.com>
2022-12-02 17:51:13 +0700

    config router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import Login from '~/pages/Login/login';
 import StoreInfor from "~/pages/StoreInfor/StoreInfor"
 import DetailProduct from '~/pages/DetailProduct/detailProduct';
 import Error404 from '~/pages/Error404/Error404';
-import Search from '~/pages/Search/serch';
+import Search from '~/pages/Search/search';
 import MyStore from '~/pages/MyStore/myStore'
 import Profile from '~/pages/Profile/profile';
 import  {UpdateInfo}  from '~/pages/Profile/updateInfo';
@@ -12,13 +12,14 @@ import  {ChangePassword}  from '~/pages/Profile/changePassword';
 import  {FavouriteProduct}  from '~/pages/Profile/favourite_product';
 
 const pageRoutes = [
-    {path:'/',component: Home,layout:true},
+    {path:'/',component: Home},
+    {path:'/login', component: Login},
     {path:'/category/:categoryID',component: Category},
-    {path:'/login',component: Login},
     {path:'/product/:productID',component: DetailProduct},
     {path: "/storeInfor",component: StoreInfor},
     {path: "/search",component: Search},
     {path: "/myStore",component: MyStore},
+    {path: "*",component: Error404},
 ]
 
 const pagePrivate = [
@@ -26,7 +27,6 @@ const pagePrivate = [
     {path: "/profile/update-info",component: UpdateInfo},
     {path: "/profile/change-pass",component: ChangePassword},
     {path: "/profile/wishlist",component: FavouriteProduct},
-    {path: "*",component: Error404},
 ]
 
 
