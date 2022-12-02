a35ef6ee9c35bd95bbe2f1ea6b016f35cc4410e3
Xuân Bách <xuanbachdevops@gmail.com>
2022-12-02 17:42:40 +0700

    update protect router

--- src/pages/PrivateRouter/privateRouter.jsx
+++ src/pages/PrivateRouter/privateRouter.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import { Navigate } from 'react-router-dom'
 
 export  const PrivateRouter = (props) => {
-    const token = window.localStorage.getItem('Token')
-    if (token) {
-        return props.children
-    } else {
-        return <Navigate to='/login'></Navigate>
-    }
+  const token = JSON.parse(localStorage.getItem('Token'))
+  if(token){
+    return props.children
+  }
+  else{
+    <Navigate to={'/login'}></Navigate>
+  }
 }
