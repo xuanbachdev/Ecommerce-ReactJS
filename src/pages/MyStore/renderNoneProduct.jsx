24747434f9cbb872cc409121e10e663c2799ad00
Xuân Bách <xuanbachdevops@gmail.com>
2022-12-02 17:53:21 +0700

    shoping cart

--- src/pages/MyStore/renderNoneProduct.jsx
+++ src/pages/MyStore/renderNoneProduct.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import styles from './myStore.module.scss'
+import {Link} from 'react-router-dom';
 function RenderNoneProduct() {
   return (
    <center>
-     <h1>Không có sản phẩm nào</h1>
+     <div className={styles.empty}>
+      <h1>Giỏ hàng của bạn trống </h1>
+      <Link to='/' className={styles.link}>Mua ngay</Link>
+     </div>
    </center>
   )
 }
