1bea10056a634e1574ecdb05b468d3705fcbd99e
Xuân Bách <xuanbachdevops@gmail.com>
2022-12-02 17:43:27 +0700

    update profile

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -1,22 +1,29 @@
 import classNames from "classnames/bind";
 import styles from '~/pages/Profile/profile.module.scss'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import  {FavouriteProduct}  from '~/pages/Profile/favourite_product';
 
 function Profile() {
     const cx = classNames.bind(styles)
+    const nav = useNavigate()
+    function update_info(){
+        nav('/profile/update-info')
+    }
+    let dataUser = JSON.parse(localStorage.getItem('User'))
     return (
         <div className={cx('wrapper')}>
-            <h3 className={cx('text-user')}>Tài khoản của {localStorage.getItem('email')}</h3>
+            <h3 className={cx('text-user')}>Tài khoản của {dataUser.username}</h3>
             <div className={cx('container')}>
                <div className={cx('content')}>
                 <div className={cx('detail')}>
                         <p className={cx('title', ['text-center'])}>Thông tin tài khoản</p>
                         <div className={cx('info')}>
-                            <p>Điểm tích luỹ của bạn: 1000 <sub>đ</sub> </p>
-                            <p>Cấp độ khách hàng: 1</p>
-                            <Link to={'update-info'}><p>Thay đổi thông tin cá nhân</p></Link>
-                            <Link to={'change-pass'}><p>Thay đổi mật khẩu</p></Link>
+                            <ul className={styles.list_menu}>
+                                <li>Điểm tích luỹ của bạn: 1000 <sub>đ</sub> </li>
+                                <li>Cấp độ khách hàng: 1</li>
+                                <li onClick={update_info}>Thay đổi thông tin cá nhân</li>
+                                <Link to={'change-pass'}><li>Thay đổi mật khẩu</li></Link>
+                            </ul>
                         </div>
                     </div>
                     <div className={cx('favorite_product')}>
