6d0912a83662981e162eab65fc84f0f9d8e485e1
Xuân Bách <xuanbachdevops@gmail.com>
2022-12-02 17:05:40 +0700

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '~/App';
+import 'react-toastify/dist/ReactToastify.css';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './components/GlobalStyles';
-import {store} from "./store"
+import {store} from "./redux/store"
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
