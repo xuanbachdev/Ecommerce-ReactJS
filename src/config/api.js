5aaafa9280bf9e68b809f8d872644c680f60e1d2
Xuân Bách <xuanbachdevops@gmail.com>
2022-12-02 17:29:22 +0700

    config api

--- src/config/api.js
+++ src/config/api.js
@@ -1,18 +1,22 @@
-import axios from "~/axios";
-let token = window.localStorage.getItem('Token')
+import axios from "~/config/axios";
 
 export const getAPI = function(url){
+    let token = JSON.parse(localStorage.getItem('Token'))
     return axios.get(url, {headers: {Authorization: token} })
 }
 export const postAPI = function(url, data){
+    let token = JSON.parse(localStorage.getItem('Token'))
     return axios.post(url,data, {headers: {Authorization: token}})
 }
 export const patchAPI = function(url, data){
+    let token = JSON.parse(localStorage.getItem('Token'))
     return axios.patch(url,data, {headers: {Authorization: token}})
 }
 export const putAPI = function(url, data){
+    let token = JSON.parse(localStorage.getItem('Token'))
     return axios.put(url,data, {headers: {Authorization: token}})
 }
 export const deleteAPI = function(url){
+    let token = JSON.parse(localStorage.getItem('Token'))
     return axios.delete(url, {headers: {Authorization: token}})
 }
