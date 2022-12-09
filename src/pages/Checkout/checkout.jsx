33713ed6aa60cca560b767b91b0510ae06366b4b
Xuân Bách <xuanbachdevops@gmail.com>
2022-12-09 09:18:28 +0700

    add checkout

--- src/pages/Checkout/checkout.jsx
+++ src/pages/Checkout/checkout.jsx
@@ -1,97 +1,97 @@
-import { useState } from 'react';
+import React from 'react';
+import { LeftOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
-export const Checkout = () => {
-	const [name, setName] = useState();
-	const [address, setAdress] = useState();
+import styles from './checkout.module.scss';
 
-	const handeleSubmit = (e) => {
-		e.preventDefault();
-		console.log(name, address);
-	};
+export const Checkout = () =>{
 	return (
-		<div>
-			<form
-				className='flex w-[50%] m-auto h-screen pt-[74px]'
-				onSubmit={handeleSubmit}
-			>
-				<div className='w-[60%] px-4'>
-					<div className='text-sm space-x-2'>
-						<div className='bg-gray-100 space-x-2 py-3 pl-4 rounded-lg'>
-							<Link to='/cart' className='text-blue-500'>
-								Giỏ hàng
-							</Link>
-							<span className='border-l-2 border-gray-300 pl-2'>
-								Thanh toán
-							</span>
-						</div>
+		<div className={styles.paymentInfo}>
+			<div className={styles.paymentInfo__header}>
+				<h3>
+					Thông tin thanh toán{' '}
+					<div className={styles.paymentInfo__header__backs}>
+						<Link to={'/myStore'}>
+							<p>
+								{' '}
+								<LeftOutlined />
+								Trở về
+							</p>
+						</Link>
 					</div>
-					<div className='space-y-4 pt-10'>
-						<div>
-							<p className='text-xl font-bold'>Thông tin giao hàng</p>
-						</div>
-						<div>
-							<input
-								type='text'
-								placeholder='Họ tên...'
-								className='py-4 px-2 border border-gray-200 w-full rounded-lg text-base text-black outline-none'
-								onChange={(e) => setName(e.target.value)}
-							/>
-						</div>
-						<div>
-							<input
-								type='text'
-								placeholder='Sđt...'
-								className='py-4 px-2 border border-gray-200 w-full rounded-lg text-base text-black outline-none'
-								onChange={(e) => setAdress(e.target.value)}
-							/>
-						</div>
-						<div>
-							<textarea
-								name=''
-								id=''
-								cols='30'
-								rows='10'
-								placeholder='Địa chỉ...'
-								className='border rounded-lg w-full p-2 outline-none'
-							></textarea>
-						</div>
-					</div>
+				</h3>
+			</div>
+			<div className={styles.paymentInfo__content}>
+				<h2>Thông tin khách hàng</h2>
+				<div className={styles.paymentInfo__content__info}>
+					<input
+						type='text'
+						className={styles.paymentInfo__content__input}
+						placeholder='Tên người nhận( bắt buộc)'
+					/>
+					<input
+						type='number'
+						className={styles.paymentInfo__content__input}
+						placeholder='Sô điện thoại( bắt buộc)'
+					/>
+					<input
+						type='email'
+						className={styles.paymentInfo__content__input}
+						placeholder='Email( bắt buộc)'
+					/>
 				</div>
-				<div className='bg-gray-100 px-8 w-[40%] pt-6 space-y-4 rounded-lg'>
-					<div className='py-6 border-t-2 flex justify-between'>
+				<h2>Chọn hình thức giao hàng</h2>
+				<div className={styles.paymentInfo__content__info1}>
+					<div className={styles.paymentInfo__content__info1__radio}>
 						<input
-							type='text'
-							placeholder='Mã giảm giá'
-							className='p-3 border rounded-lg border-gray-200'
+							type='radio'
+							name='check'
+							className={styles.paymentInfo__content__input__radio}
+						/>{' '}
+						Nhận tại cửa hàng
+					</div>
+					<div className={styles.paymentInfo__content__info1__radio}>
+						<input
+							type='radio'
+							name='check'
+							className={styles.paymentInfo__content__input__radio}
 						/>
-						<button className='py-3 px-8 bg-blue-400 text-white  rounded-lg hover:bg-blue-800'>
-							Sử dụng
-						</button>
-					</div>
-					<div className='py-6 border-t-2 flex justify-between'>
-						<p className='font-semibold'>Phí vận chuyển</p>
-						<p className='font-semibold'>Miễn phí vận chuyển</p>
-					</div>
-					<div className='py-6 border-t-2 flex justify-between'>
-						<p className='font-bold'>Tổng cộng</p>
-						<p className='space-x-1'>
-							<span>VNĐ</span>
-							<span>859000</span>
-							<span>đ</span>
-						</p>
-					</div>
-					<div className='border border-orange-600 py-4 px-2'>
-						Chúng tôi sẽ XÁC NHẬN đơn hàng bằng TIN NHẮN SMS hoặc GỌI ĐIỆN. Bạn
-						vui lòng kiểm tra TIN NHẮN hoặc NGHE MÁY ngay khi đặt hàng thành
-						công và CHỜ NHẬN HÀNG
-					</div>
-					<div className='float-right hover:bg-blue-800'>
-						<button className='px-6 py-4 bg-blue-500 text-lg rounded-lg text-white'>
-							Hoàn tất đơn hàng
-						</button>
+						Giao tận nơi
 					</div>
 				</div>
-			</form>
+				<input
+					type='text'
+					className={styles.paymentInfo__content__input}
+					placeholder='Địa chỉ cụ thể( bắt buộc)'
+				/>
+				<input
+					type='text'
+					className={styles.paymentInfo__content__input}
+					placeholder='Mã giảm giá( không bắt buộc)'
+				/>
+			</div>
+			<div className={styles.Cart__content__list__buy1}>
+				<div className={styles.Cart__content__list__buy1__price}>
+					<p>Tổng tiền tạm tính: </p>
+					<p>
+						{(window.localStorage.getItem('totalPrice')).toLocaleString(
+							'vi-VN',
+							{ style: 'currency', currency: 'VND' },
+						)}
+					</p>
+				</div>
+				<div className={styles.Cart__content__list__buy1__btns}>
+					<Link>
+						{' '}
+						<p className={styles.Cart__content__list__buy1__btn1}>Đặt hàng</p>
+					</Link>
+					<Link to={'/'}>
+						{' '}
+						<p className={styles.Cart__content__list__buy1__btn2}>
+							Chọn thêm sản phẩm khác
+						</p>
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
-};
+}
